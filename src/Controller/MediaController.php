255bce6132597be4b8ae3fd24a3c0861932eafc9
ajiredun <ajir.edun@gmail.com>
2019-09-20 13:31:25 +0000

    ajout media

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Enums\Roles;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/media")
@@ -12,12 +14,27 @@ class MediaController extends AbstractController
 {
     /**
      * @Route("/{type}", name="rf_media")
+     * @IsGranted(Roles::ROLE_VIEWER)
      */
     public function index($type)
     {
-        if ($type=="files") {
-            //$this->redirectToRoute('rf_user_profile', ['id' => $user->getId(), 'rfsuccess' => $user->getName() . ' details has been updated successfully']);
+        $instanceFiles = 'files_view';
+        $instanceGallery = 'gallery_view';
+
+        if ($this->isGranted(Roles::ROLE_MEDIA_MANAGEMENT_EDITOR)) {
+            $instanceFiles = 'files';
+            $instanceGallery = 'gallery';
         }
+
+        if ($type=="files") {
+            return $this->redirectToRoute('elfinder', ['instance' => $instanceFiles]);
+        }
+
+        if ($type=="gallery") {
+            return $this->redirectToRoute('elfinder', ['instance' => $instanceGallery]);
+        }
+
+        return $this->redirectToRoute('rf_dashboard', ['id' => $user->getId(), 'rfsuccess' => $user->getName() . ' details has been updated successfully']);
     }
 
 }
